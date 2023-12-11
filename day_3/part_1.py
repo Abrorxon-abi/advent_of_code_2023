798a4901a52f455015bffaae6a39029b803f7761
Abrorxon-abi <ppvbuyppv@gmail.com>
2023-12-11 09:37:34 +0500

    solved 3rd days part one puzzle

---
+++ day_3/part_1.py
@@ -0,0 +1,35 @@
+import re
+
+f = open("puzzle_input.txt", "r")
+numPositions = []
+inputList = []
+
+lines = f.read().splitlines()
+
+
+def FindSymbols(startX: int, startY: int, lenght: int):
+    flag = False
+    for i in range(-1, 2):
+        for j in range(-1, lenght+1):
+            if (startY+i >= 0 and startY+i < len(inputList) and startX+j >= 0 and startX+j < len(inputList[startY+i])):
+                if (re.match(r'[^\d\.\n]', inputList[startY+i][startX+j])):
+                    flag = True
+    return flag
+
+
+for x in lines:
+    inputList.append(x)
+    tempList = []
+    for y in re.finditer(r"\d+", x):
+        z = [y.start(), int(y.end()-y.start()), int(y.group())]
+        tempList.append(z)
+    numPositions.append(tempList)
+
+validNums = 0
+
+for index, posRow in enumerate(numPositions):
+    for numInfo in posRow:
+        if (FindSymbols(numInfo[0], index, numInfo[1])):
+            validNums += numInfo[2]
+
+print(validNums)
