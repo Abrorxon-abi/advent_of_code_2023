032d17d270a569eb3897e3559479f3abafc8b433
Abrorxon-abi <ppvbuyppv@gmail.com>
2023-12-12 09:38:13 +0500

    solved day 6th part one puzzle

---
+++ day_6/part_1.py
@@ -0,0 +1,20 @@
+import re
+import math
+
+f = open("puzzle_input.txt", "r")
+lines = f.read().splitlines()
+f.close()
+
+timeList = list(map(int, re.findall(r'[\d]+', lines[0])))
+distList = list(map(int, re.findall(r'[\d]+', lines[1])))
+
+total = 1
+
+for x, y in zip(timeList, distList):
+    lowBound = 0
+    for lowBound in range(math.ceil(x/2)):
+        if lowBound * (x-lowBound) > y:
+            break
+    total *= (x - 2*lowBound)+1
+
+print(total)
